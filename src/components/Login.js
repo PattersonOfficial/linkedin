d38f77c4f5326192a226f7888fd6f56df3395a93
PattersonOfficial <akrongwork@gmail.com>
2021-07-16 16:04:09 +0000

    Login page looking great

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,7 +7,17 @@ const Login = (props) => {
         <a href='/'>
           <img src='/images/login-logo.svg' alt='' />
         </a>
+        <div>
+          <Join>Join now</Join>
+          <SignIn>Sign in</SignIn>
+        </div>
       </Nav>
+
+      <Section>
+        <Hero>
+          <h1>Welcome To Your Professional Community</h1>
+        </Hero>
+      </Section>
     </Container>
   );
 };
@@ -16,20 +26,88 @@ const Container = styled.div`
   padding: 0px;
 `;
 
-const Nav = styled.div`
-  max-with: 1128px;
+const Nav = styled.nav`
+  max-width: 1128px;
   margin: auto;
   padding: 12px 0 16px;
   display: flex;
   align-items: center;
   justify-content: space-between;
   flex-wrap: nowrap;
-
   & > a {
     width: 135px;
     height: 34px;
     @media (max-with: 768px) {
       padding: 0 5px;
+    }
+  }
+`;
+
+const Join = styled.a`
+  font-size: 16px;
+  padding: 10px 12px;
+  text-decoration: none;
+  color: rgba(0, 0, 0, 0.6);
+  margin-right: 12px;
+  border-radius: 24px;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.08);
+    color: rgba(0, 0, 0, 0.9);
+    text-decoration: none;
+  }
+`;
+
+const SignIn = styled.a`
+  box-shadow: inset 0 0 0 1px #0a66c2;
+  color: #0a66c2;
+  border-radius: 24px;
+  transition-duration: 167ms;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 40px;
+  padding: 10px 24px;
+  text-align: center;
+  background-color: rgba(0, 0, 0, 0);
+  &:hover {
+    background-color: rgba(112, 181, 249, 0.15);
+    color: #0a66c2;
+    text-decoration: none;
+  }
+`;
+
+const Section = styled.section`
+  display: flex;
+  align-content: start;
+  min-height: 700px;
+  padding-bottom: 138px;
+  padding-top: 40px;
+  padding: 60px 0;
+  position: relative;
+  flex-wrap: wrap;
+  width: 100%;
+  max-width: 1128px;
+  align-items: center;
+  margin: auto;
+  @media (max-width: 768px) {
+    margin: auto;
+    min-height: 0px;
+  }
+`; 
+
+const Hero = styled.div`
+  width: 100%;
+  h1 {
+    padding-bottom: 0;
+    width: 55%;
+    font-size: 56px;
+    color: #2977c9;
+    font-weight: 200;
+    line-height: 70px;
+    @media (max-width: 768px) {
+      text-align: center;
+      font-size: 20px;
+      width: 100%;
+      line-height: 2; 
     }
   }
 `;
