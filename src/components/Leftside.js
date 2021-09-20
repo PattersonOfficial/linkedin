cd987f4d0c6e2d2a481834f65d22bfc8295e04d7
PattersonOfficial <akrongwork@gmail.com>
2021-09-20 20:52:38 +0000

    Home page component being readied

---
+++ src/components/Leftside.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Leftside = (props) => {
+  return <Container>Left Panel</Container>;
+};
+
+const Container = styled.div``;
+
+export default Leftside;
---
+++ src/components/Rightside.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Rightside = (props) => {
+    return (
+        <Container>
+            Right Panel
+        </Container>
+    );
+}
+
+const Container = styled.div``;
+
+
+export default Rightside;
